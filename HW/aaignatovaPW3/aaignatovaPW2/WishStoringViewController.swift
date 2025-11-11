f7ec7ac81d43563b9519b98fe4c34235ab928f10
TwTYwY <aaignatova@edu.hse.ru>
2025-11-11 16:42:56 +0300

    HW3: Point 3

--- HW/aaignatovaPW3/aaignatovaPW2/WishStoringViewController.swift
+++ HW/aaignatovaPW3/aaignatovaPW2/WishStoringViewController.swift
@@ -6,8 +6,49 @@
 //
 
 import UIKit
+
+extension UIView {
+    func pin(to view: UIView, _ insets: UIEdgeInsets = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: view.topAnchor, constant: insets.top),
+            leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: insets.left),
+            trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -insets.right),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -insets.bottom)
+        ])
+    }
+}
+
 final class WishStoringViewController: UIViewController {
+    struct Constants {
+        static let tableCornerRadius: CGFloat = 15
+        static let tableOffset: UIEdgeInsets = .init(top: 100, left: 20, bottom: 20, right: 20)
+    }
+    private let table: UITableView = UITableView(frame: .zero)
+    
     override func viewDidLoad() {
         view.backgroundColor = .blue
+        configureTable()
+    }
+    
+    private func configureTable() {
+        view.addSubview(table)
+        table.backgroundColor = .red
+        table.dataSource = self
+        table.separatorStyle = .none
+        table.layer.cornerRadius = Constants.tableCornerRadius
+        
+        table.pin(to: view, Constants.tableOffset)
+    }
+}
+
+extension WishStoringViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
