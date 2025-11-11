caad253682bb1df1fb914c7027ce1e2b8cc7de49
TwTYwY <aaignatova@edu.hse.ru>
2025-11-11 15:58:23 +0300

    HW 3: Point 1

---
+++ seminar/albumCovers/albumCovers/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  albumCovers
+//
+//  Created by Anzhelika Ignatova on 07.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private var collectionView: UICollectionView!
+    
+    private let coversNames: [String] = ["album1", "album2", "album3", "album4", "album5", "album_6", "album7", "album8", "album9"]
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupCollectionView()
+        }
+        
+        private func setupCollectionView() {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            
+            collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            collectionView.translatesAutoresizingMaskIntoConstraints = false
+            collectionView.delegate = self
+            collectionView.dataSource = self
+            collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+            
+            view.addSubview(collectionView)
+            
+            NSLayoutConstraint.activate([
+                collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+                collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+        }
+    }
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return coversNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemCyan
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 180, height: 180)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+}
+
