2b51aadc99ac96138651886a5801bf7e8bedfae8
TwTYwY <aaignatova@edu.hse.ru>
2025-11-14 00:02:32 +0300

    HW3: Points 6-8

--- HW/aaignatovaPW3/aaignatovaPW2/WishStoringViewController.swift
+++ HW/aaignatovaPW3/aaignatovaPW2/WishStoringViewController.swift
@@ -43,12 +43,22 @@ final class WishStoringViewController: UIViewController {
         static let tableCornerRadius: CGFloat = 15
         static let tableOffset: UIEdgeInsets = .init(top: 100, left: 20, bottom: 20, right: 20)
         static let sectionsCount: Int = 2
+        static let wishesKey = "wishesKey"
     }
+    private let defaults = UserDefaults.standard
     private let table: UITableView = UITableView(frame: .zero)
-    private var wishArray: [String] = ["I wish to add cells to the table"]
+    private var wishArray: [String] = []
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         view.backgroundColor = .blue
+        
+        if let savedWishes = defaults.array(forKey: Constants.wishesKey) as? [String] {
+            wishArray = savedWishes
+        } else {
+            wishArray = ["I wish to add cells to the table"]
+        }
+        
         configureTable()
     }
     
@@ -58,6 +68,9 @@ final class WishStoringViewController: UIViewController {
         table.dataSource = self
         table.separatorStyle = .none
         table.layer.cornerRadius = Constants.tableCornerRadius
+        
+        table.allowsSelection = false
+        table.isUserInteractionEnabled = true
         
         table.pin(to: view, Constants.tableOffset)
         table.register(WrittenWishCell.self, forCellReuseIdentifier: WrittenWishCell.reuseId)
@@ -96,6 +109,16 @@ extension WishStoringViewController: UITableViewDataSource {
                 return cell
             }
             
+            addWishCell.addWish = { [weak self] wish in
+                guard let self = self else { return }
+                if self.wishArray == ["I wish to add cells to the table"] {
+                    self.wishArray.removeAll()
+                }
+                self.wishArray.append(wish)
+                self.defaults.set(self.wishArray, forKey: Constants.wishesKey)
+                self.table.reloadData()
+            }
+            
             return addWishCell
             
         case 1:
@@ -116,6 +139,20 @@ extension WishStoringViewController: UITableViewDataSource {
             return UITableViewCell()
         }
     }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.section == 1
+    }
+
+    func tableView(_ tableView: UITableView,
+                   commit editingStyle: UITableViewCell.EditingStyle,
+                   forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete && indexPath.section == 1 {
+            wishArray.remove(at: indexPath.row)
+            defaults.set(wishArray, forKey: Constants.wishesKey)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+
 }
 
 final class WrittenWishCell: UITableViewCell {
@@ -175,6 +212,29 @@ final class WrittenWishCell: UITableViewCell {
 
 final class AddWishCell: UITableViewCell {
     static let reuseId: String = "AddWishCell"
+    
+    private enum Constants {
+        static let wrapColor: UIColor = .white
+        static let wrapRadius: CGFloat = 16
+        static let wrapOffsetV: CGFloat = 5
+        static let wrapOffsetH: CGFloat = 10
+        static let textViewColor: UIColor = .lightGray
+        static let textViewRadius: CGFloat = 8
+        static let textViewOffset: CGFloat = 8
+        static let textViewFont: UIFont = .systemFont(ofSize: 16)
+        static let textViewContainerSize: UIEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        static let buttonText: String = "Add Wish"
+        static let buttonColor: UIColor = .white
+        static let buttonTextColor: UIColor = .systemPink
+        static let buttonH: CGFloat = 45
+        static let buttonOffset: CGFloat = 8
+    }
+    
+    var addWish: ((String) -> ())?
+    
+    private let textView: UITextView = UITextView()
+    private let addButton: UIButton = UIButton(type: .system)
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         configureUI()
@@ -186,6 +246,96 @@ final class AddWishCell: UITableViewCell {
     }
         
     private func configureUI() {
-        backgroundColor = .green
+        selectionStyle = .none
+        backgroundColor = .clear
+        
+        let wrap: UIView = UIView()
+        contentView.addSubview(wrap)
+        
+        wrap.backgroundColor = Constants.wrapColor
+        wrap.layer.cornerRadius = Constants.wrapRadius
+        wrap.pinVertical(to: contentView, top: Constants.wrapOffsetV, bottom: Constants.wrapOffsetH)
+        wrap.pinHorizontal(to: contentView, left: Constants.wrapOffsetH, right: Constants.wrapOffsetH)
+        
+        textView.isUserInteractionEnabled = true
+        textView.isEditable = true
+        textView.isSelectable = true
+        
+        wrap.addSubview(textView)
+        
+        textView.backgroundColor = Constants.textViewColor
+        textView.layer.cornerRadius = Constants.textViewRadius
+        textView.font = Constants.textViewFont
+        textView.textContainerInset = Constants.textViewContainerSize
+        textView.isScrollEnabled = true
+        textView.isEditable = true
+        textView.text = "Enter your wish here..."
+        textView.textColor = .darkGray
+        
+        setupTextView()
+        
+        wrap.addSubview(addButton)
+        
+        addButton.setTitle(Constants.buttonText, for: .normal)
+        addButton.backgroundColor = Constants.buttonColor
+        addButton.setTitleColor(Constants.buttonTextColor, for: .normal)
+        addButton.layer.cornerRadius = Constants.textViewRadius
+        addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+        
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+                
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: wrap.topAnchor, constant: Constants.textViewOffset),
+            textView.leadingAnchor.constraint(equalTo: wrap.leadingAnchor, constant: Constants.textViewOffset),
+            textView.trailingAnchor.constraint(equalTo: wrap.trailingAnchor, constant: -Constants.textViewOffset),
+            textView.heightAnchor.constraint(equalToConstant: 80),
+                    
+            addButton.topAnchor.constraint(equalTo: textView.bottomAnchor, constant: Constants.buttonOffset),
+            addButton.leadingAnchor.constraint(equalTo: wrap.leadingAnchor, constant: Constants.textViewOffset),
+            addButton.trailingAnchor.constraint(equalTo: wrap.trailingAnchor, constant: -Constants.textViewOffset),
+            addButton.bottomAnchor.constraint(equalTo: wrap.bottomAnchor, constant: -Constants.buttonOffset),
+            addButton.heightAnchor.constraint(equalToConstant: Constants.buttonH)
+        ])
+    }
+    
+    private func setupTextView() {
+        textView.delegate = self
+    }
+    
+    @objc private func activateTextView() {
+        textView.becomeFirstResponder()
+        if textView.textColor == .darkGray && textView.text == "Enter your wish here..." {
+            textView.text = ""
+            textView.textColor = .black
+        }
+    }
+    
+    @objc private func addButtonTapped() {
+        let wishText = textView.textColor == .darkGray ? "" : textView.text
+        guard let wish = wishText, !wish.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else {
+            return
+        }
+                
+        addWish?(wish)
+        textView.text = "Enter your wish here..."
+        textView.textColor = .darkGray
+        textView.resignFirstResponder()
+    }
+}
+
+extension AddWishCell: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == .darkGray && textView.text == "Enter your wish here..." {
+            textView.text = ""
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = "Enter your wish here..."
+            textView.textColor = .darkGray
+        }
     }
 }
